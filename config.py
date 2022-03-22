a42660faf1c838e7a8489c23cc483d67a47537da
Matreshkarp <94852791+Matreshkarp@users.noreply.github.com>
2022-03-22 21:05:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+# Слито в https://t.me/HACKER_PHONE_VIP
 
-qiwi = "Киви" # Ваш QIWI
-yandex_money = "Яндекс деньги" # Ваш ЮMoney
-payeer = "Пеер" # Ваш PAYEER
+Admin = "https://t.me/TbI_KPbIM" # Ссылка на админа
+token = "5121856782:AAFi9qvmLIOI-lG9PyXCG359VAM83Gj-b30" # Токен бота с @BotFather
+
+qiwi = "GhostRiver" # Ваш QIWI
+yandex_money = "Только киви" # Ваш ЮMoney
+payeer = "Только Киви" # Ваш PAYEER
 
 # Слито в https://t.me/HACKER_PHONE_VIP
